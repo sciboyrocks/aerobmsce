8b8f12adb6d2d897329b8902d29efbe8cc44121f
Samrudh Raikote <128334557+sciboyrocks@users.noreply.github.com>
2025-05-26 10:53:40 +0000

    README.md Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   </StrictMode>,
 )
